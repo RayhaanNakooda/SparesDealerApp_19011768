5697ff6b456d3c9dc179265620e52e0c942dd389
Rayhaan <rayhaannak1@gmail.com>
2021-05-14 15:49:47 +0200

    first commit

---
+++ SparesDealerApp/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparesDealerApp
+{
+   public class Vehicle
+    {
+
+        private string model;
+        private string make;
+        private double price;
+        private int numSold;
+
+        public Vehicle(int numSold)
+        {
+            this.numSold = numSold;
+        }
+
+        public Vehicle(string model, string make, double price, int numSold)
+        {
+            this.model = model;
+            this.make = make;
+            this.price = price;
+            this.numSold = numSold;
+        }
+
+
+        public string Model { get => model; set => model = value; }
+        public string Make { get => make; set => make = value; }
+        public double Price { get => price; set => price = value; }
+        public int NumSold { get => numSold; set => numSold = value; }
+    }
+}
---
+++ SparesDealerApp/Worker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparesDealerApp
+{
+    class Worker
+    {
+
+        //Creating an instance of the Worker Class, the class contains a List of type Vehicle.
+        private static Worker instance;
+
+
+        //Only one of these lists are allowed to be created via the Singleton
+        public static List<Vehicle> vehicles = new List<Vehicle>();
+
+        
+        //A private Constructor 
+        private Worker() { }
+
+       
+
+        //So basically in this method I have an instance of the worker class which contains and stores the List of Vehicle Spares, so this method thats the instance variable
+        //and checks if the instance exists and if it is null then the instance of the worker class is created which can add Vehicles to the list
+        public static Worker GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Worker();
+            }
+
+            return instance;
+        }
+
+        public void availableVehicleSpares(Vehicle v) {
+
+            vehicles.Add(v);
+
+        }
+
+
+    }
+}
